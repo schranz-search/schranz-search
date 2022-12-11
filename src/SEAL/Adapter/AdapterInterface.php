0c46e077799fed2f1f8119c07a2442563f003388
Alexander Schranz <alexander@sulu.io>
2022-12-11 17:10:48 +0100

    Remove unnecessary method from adapter and make total a method (#6)

--- src/SEAL/Adapter/AdapterInterface.php
+++ src/SEAL/Adapter/AdapterInterface.php
@@ -2,17 +2,9 @@
 
 namespace Schranz\Search\SEAL\Adapter;
 
-use Schranz\Search\SEAL\Search\Result;
-use Schranz\Search\SEAL\Search\Search;
-
 interface AdapterInterface
 {
     public function getSchemaManager(): SchemaManagerInterface;
 
     public function getConnection(): ConnectionInterface;
-
-    /**
-     * @return iterable<array<string, mixed>>
-     */
-    public function search(Search $searchBuilder): Result;
 }
