1045524f91e17871d50530d375a24db8736c4775
Alexander Schranz <alexander@sulu.io>
2022-12-11 17:09:05 +0100

    Remove unnecessary method from adapter and make total a method

--- src/SEAL/Search/Result.php
+++ src/SEAL/Search/Result.php
@@ -2,15 +2,20 @@
 
 namespace Schranz\Search\SEAL\Search;
 
-final class Result extends \IteratorIterator
+class Result extends \IteratorIterator
 {
     /**
      * @param \Generator<array<string, mixed>> $documents
      */
     public function __construct(
         \Generator $documents,
-        readonly public int $total,
+        readonly private int $total,
     ) {
         parent::__construct($documents);
+    }
+
+    public function total(): int
+    {
+        return $this->total;
     }
 }
