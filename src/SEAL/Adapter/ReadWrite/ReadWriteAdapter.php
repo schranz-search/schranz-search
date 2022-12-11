0dfce9ab45d417b6a29c09d642d2e1db32799085
Alexander Schranz <alexander@sulu.io>
2022-12-11 17:56:26 +0100

    Add ReadWrite Adapter (#7)

---
+++ src/SEAL/Adapter/ReadWrite/ReadWriteAdapter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Schranz\Search\SEAL\Adapter\ReadWrite;
+
+use Schranz\Search\SEAL\Adapter\AdapterInterface;
+use Schranz\Search\SEAL\Adapter\ConnectionInterface;
+use Schranz\Search\SEAL\Adapter\SchemaManagerInterface;
+
+final class ReadWriteAdapter implements AdapterInterface
+{
+    private ?ConnectionInterface $connection = null;
+
+    public function __construct(
+        private readonly AdapterInterface $readAdapter,
+        private readonly AdapterInterface $writeAdapter,
+    ) {}
+
+    public function getSchemaManager(): SchemaManagerInterface
+    {
+        return $this->writeAdapter->getSchemaManager();
+    }
+
+    public function getConnection(): ConnectionInterface
+    {
+        if ($this->connection === null) {
+            return new ReadWriteConnection(
+                $this->readAdapter->getConnection(),
+                $this->writeAdapter->getConnection(),
+            );
+        }
+
+        return $this->connection;
+    }
+}
