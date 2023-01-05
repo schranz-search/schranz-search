daed79f99f9f9d7d1e79c0bdfa20e9b016be5b55
Alexander Schranz <alexander@sulu.io>
2023-01-06 00:26:55 +0100

    Use Sync Task instead of sleep for Elasticsearch and Opensearch Adapters

--- packages/seal-opensearch-adapter/Tests/OpensearchConnectionTest.php
+++ packages/seal-opensearch-adapter/Tests/OpensearchConnectionTest.php
@@ -19,14 +19,4 @@ class OpensearchConnectionTest extends AbstractConnectionTestCase
 
         parent::setUpBeforeClass();
     }
-
-    public static function waitForAddDocuments(): void
-    {
-        usleep((int) ($_ENV['OPENSEARCH_WAIT_TIME'] ?? 100_000));
-    }
-
-    public static function waitForDeleteDocuments(): void
-    {
-        usleep((int) ($_ENV['OPENSEARCH_WAIT_TIME'] ?? 100_000));
-    }
 }
