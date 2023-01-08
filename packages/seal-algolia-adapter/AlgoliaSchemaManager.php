e72e64f949ceb1183dd7a2f9da469c2467d65890
Alexander Schranz <alexander@sulu.io>
2023-01-08 22:18:47 +0100

    Add searchable false to Algolia Adapter

--- packages/seal-algolia-adapter/AlgoliaSchemaManager.php
+++ packages/seal-algolia-adapter/AlgoliaSchemaManager.php
@@ -4,6 +4,7 @@ namespace Schranz\Search\SEAL\Adapter\Algolia;
 
 use Algolia\AlgoliaSearch\SearchClient;
 use Schranz\Search\SEAL\Adapter\SchemaManagerInterface;
+use Schranz\Search\SEAL\Schema\Field;
 use Schranz\Search\SEAL\Schema\Index;
 use Schranz\Search\SEAL\Task\AsyncTask;
 use Schranz\Search\SEAL\Task\TaskInterface;
@@ -41,12 +42,12 @@ final class AlgoliaSchemaManager implements SchemaManagerInterface
     {
         $searchIndex = $this->client->initIndex($index->name);
 
-        $indexResponse = $searchIndex->setSettings([
-            'searchableAttributes' => [],
-            'attributesForFaceting' => [
-                $index->getIdentifierField()->name,
-            ],
-        ]);
+        $attributes = $this->getAttributes($index->fields);
+        $attributes['attributesForFaceting'] = [
+            $index->getIdentifierField()->name
+        ];
+
+        $indexResponse = $searchIndex->setSettings($attributes);
 
         if (true !== ($options['return_slow_promise_result'] ?? false)) {
             return null;
@@ -55,5 +56,47 @@ final class AlgoliaSchemaManager implements SchemaManagerInterface
         return new AsyncTask(function() use ($indexResponse) {
             $indexResponse->wait();
         });
+    }
+
+    /**
+     * @param Field\AbstractField[] $fields
+     *
+     * @return array{
+     *     searchableAttributes: array<string>,
+     * }
+     */
+    private function getAttributes(array $fields): array
+    {
+        $attributes = [
+            'searchableAttributes' => [],
+        ];
+
+        foreach ($fields as $name => $field) {
+            if ($field instanceof Field\ObjectField) {
+                foreach ($this->getAttributes($field->fields) as $attributeType => $fieldNames) {
+                    foreach ($fieldNames as $fieldName) {
+                        $attributes[$attributeType][] = $name . '.' . $fieldName;
+                    }
+                }
+
+                continue;
+            } elseif ($field instanceof Field\TypedField) {
+                foreach ($field->types as $type => $fields) {
+                    foreach ($this->getAttributes($fields) as $attributeType => $fieldNames) {
+                        foreach ($fieldNames as $fieldName) {
+                            $attributes[$attributeType][] = $name . '.' . $type . '.' . $fieldName;
+                        }
+                    }
+                }
+
+                continue;
+            }
+
+            if ($field->searchable) {
+                $attributes['searchableAttributes'][] = $name;
+            }
+        }
+
+        return $attributes;
     }
 }
