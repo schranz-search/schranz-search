712193b2d054fc86df6b0bdc93a36babb4aa97eb
Alexander Schranz <alexander@sulu.io>
2023-02-16 21:06:33 +0100

    Type none text fields searchable as false

--- packages/seal/Schema/Field/DateTimeField.php
+++ packages/seal/Schema/Field/DateTimeField.php
@@ -8,6 +8,7 @@ namespace Schranz\Search\SEAL\Schema\Field;
 final class DateTimeField extends AbstractField
 {
     /**
+     * @param false $searchable
      * @param array<string, mixed> $options
      */
     public function __construct(
