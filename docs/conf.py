68fdc5e6332eefe79d320960142ed84bc7c263b9
Alexander Schranz <alexander@sulu.io>
2023-04-14 01:14:29 +0200

    Add documentation build (#149)

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SEAL'
+copyright = '2023, Alexander Schranz'
+author = 'Alexander Schranz'
+release = '0.1'
+html_title = 'SEAL Documentation'
+html_favicon = '_static/icons/favicon.ico'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_tabs.tabs']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = "furo"
+
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+
+# Tabs
+sphinx_tabs_disable_tab_closing = True
---
+++ docs/cookbooks/index.rst
@@ -0,0 +1,13 @@
+Cookbooks
+=========
+
+This section should contain in future a collection of recipes for common tasks.
+
+Let us know via the issue tracker on our `Github Repository <https://github.com/schranz-search/schranz-search>`_.
+if you have a recipe you would like to see here.
+
+This can go over creating a basic search page in your favorite framework,
+over modifying default settings of an Index, mapping data to a Model class
+and over connecting Index updates to your favorite ``ORM`` or persistent layer.
+
+* Create your own Adapter (WIP)
