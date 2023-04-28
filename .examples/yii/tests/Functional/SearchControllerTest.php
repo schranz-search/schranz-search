297a5080c046b1a43f5e2142c4a6657d4a25074c
Alexander Schranz <alexander@sulu.io>
2023-04-28 16:36:37 +0200

    Add tests and update CI and package configurations

---
+++ .examples/yii/tests/Functional/SearchControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Yiisoft\Yii\Testing\FunctionalTester;
+
+final class SearchControllerTest extends TestCase
+{
+    private FunctionalTester $tester;
+
+    protected function setUp(): void
+    {
+        $this->tester = new FunctionalTester();
+    }
+
+    public function testSearch(): void
+    {
+        $this->tester->bootstrapApplication(\dirname(__DIR__, 2));
+        $response = $this->tester->doRequest('GET', '/');
+
+        self::assertSame(200, $response->getStatusCode());
+        $content = $response->getContent();
+        self::assertStringContainsString('<title>Search Engines</title>', $content);
+
+        $crawler = $this->crawler($content);
+        $crawler->filter('a')->each(function ($node) {
+            $response = $this->tester->doRequest('GET', $node->attr('href'));
+            self::assertSame(200, $response->getStatusCode());
+            $content = $response->getContent();
+
+            $crawler = $this->crawler($content);
+
+            $this->assertStringContainsString($node->text(), $crawler->filter('title')->first()->text());
+
+            $h1s = $crawler->filter('h1');
+            $this->assertCount(1, $h1s);
+
+            $h1 = $h1s->first();
+
+            $this->assertStringContainsString(\str_replace('-', '', (string) $node->text()), $h1->text());
+        });
+    }
+
+    private function crawler(string $content): Crawler
+    {
+        return new Crawler($content);
+    }
+}
