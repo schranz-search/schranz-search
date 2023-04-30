1301061277aec6f9ee8f4f5b6ea17aa892bf75f2
Alexander Schranz <alexander@sulu.io>
2023-04-30 19:17:50 +0200

    Add license header to schranz search packages

--- integrations/yii/config/di-console.php
+++ integrations/yii/config/di-console.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Schranz Search package.
+ *
+ * (c) Alexander Schranz <alexander@sulu.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Psr\Container\ContainerInterface;
 use Schranz\Search\Integration\Yii\Command\IndexCreateCommand;
 use Schranz\Search\Integration\Yii\Command\IndexDropCommand;
--- packages/seal-algolia-adapter/bin/drop_all_indexes.php
+++ packages/seal-algolia-adapter/bin/drop_all_indexes.php
@@ -1,7 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-// internal script to cleanup old algolia indices because of
+/*
+ * This file is part of the Schranz Search package.
+ *
+ * (c) Alexander Schranz <alexander@sulu.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 // https://github.com/schranz-search/schranz-search/issues/82
 
 /** @internal */
