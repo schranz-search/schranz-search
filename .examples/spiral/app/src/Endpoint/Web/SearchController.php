45b056643aa3e5cffaff3c5441aaf34def581809
Alexander Schranz <alexander@sulu.io>
2023-08-25 01:54:55 +0200

    Fix integration issue

--- .examples/spiral/app/src/Endpoint/Web/SearchController.php
+++ .examples/spiral/app/src/Endpoint/Web/SearchController.php
@@ -143,7 +143,7 @@ final class SearchController
     }
 
     #[Route(route: '/loupe', name: 'loupe')]
-    public function elasticsearch(): string
+    public function loupe(): string
     {
         $class = $this->getAdapterClass($this->loupeEngine);
 
