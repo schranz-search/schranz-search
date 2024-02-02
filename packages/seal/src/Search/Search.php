02e74314247d2cf8dcd73ce363337048420eba69
Alexander Schranz <alexander@sulu.io>
2024-02-02 19:03:22 +0100

    Add highlight methods to search and search builder

--- packages/seal/src/Search/Search.php
+++ packages/seal/src/Search/Search.php
@@ -21,6 +21,7 @@ final class Search
      * @param array<string, Index> $indexes
      * @param object[] $filters
      * @param array<string, 'asc'|'desc'> $sortBys
+     * @param array<string> $higlightFields
      */
     public function __construct(
         public readonly array $indexes = [],
@@ -28,6 +29,9 @@ final class Search
         public readonly array $sortBys = [],
         public readonly ?int $limit = null,
         public readonly int $offset = 0,
+        public readonly array $higlightFields = [],
+        public readonly string $highlightPreTag = '<mark>',
+        public readonly string $highlightPostTag = '</mark>',
     ) {
     }
 }
