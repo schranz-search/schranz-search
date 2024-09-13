bd0ecc65ab12c3c06a802c87c459839f29182489
Alexander Schranz <alexander@sulu.io>
2024-09-13 09:47:00 +0200

    Fix Yii compatibility issues with dev packages in the example project (#417)

--- .examples/yii/src/ViewInjection/CommonViewInjection.php
+++ .examples/yii/src/ViewInjection/CommonViewInjection.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\ViewInjection;
 
 use App\ApplicationParameters;
-use Yiisoft\Yii\View\CommonParametersInjectionInterface;
+use Yiisoft\Yii\View\Renderer\CommonParametersInjectionInterface;
 
 final class CommonViewInjection implements CommonParametersInjectionInterface
 {
