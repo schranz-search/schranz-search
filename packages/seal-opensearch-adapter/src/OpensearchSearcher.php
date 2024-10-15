1b7a0433811bc4e7591598da1b7d950c4a5e29d9
Alexander Schranz <alexander@sulu.io>
2024-10-15 09:05:13 +0200

    Add InCondition Opensearch Adapter

--- packages/seal-opensearch-adapter/src/OpensearchSearcher.php
+++ packages/seal-opensearch-adapter/src/OpensearchSearcher.php
@@ -173,6 +173,7 @@ final class OpensearchSearcher implements SearcherInterface
                 $filter instanceof Condition\GreaterThanEqualCondition => $filterQueries[]['range'][$this->getFilterField($indexes, $filter->field)]['gte'] = $filter->value,
                 $filter instanceof Condition\LessThanCondition => $filterQueries[]['range'][$this->getFilterField($indexes, $filter->field)]['lt'] = $filter->value,
                 $filter instanceof Condition\LessThanEqualCondition => $filterQueries[]['range'][$this->getFilterField($indexes, $filter->field)]['lte'] = $filter->value,
+                $filter instanceof Condition\InCondition => $filterQueries[]['terms'][$this->getFilterField($indexes, $filter->field)] = $filter->values,
                 $filter instanceof Condition\GeoDistanceCondition => $filterQueries[]['geo_distance'] = [
                     'distance' => $filter->distance,
                     $this->getFilterField($indexes, $filter->field) => [
